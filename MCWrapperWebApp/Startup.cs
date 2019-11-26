9ec9bc050ef3d9b2efada45fa9eacb71d8410cd5
scanlifter <ryan@ryangoodwin.dev>
2019-11-25 22:27:02 -0700

    MCWrapper update

--- MCWrapperWebApp/Startup.cs
+++ MCWrapperWebApp/Startup.cs
@@ -34,9 +34,31 @@ namespace MCWrapperWebApp
             // scenario, https://mcwrapper.com/configure-options
             // 
             // IConfiguration or explicit configuration constructors are available as well.
-            services.AddMultiChainCoreRpcServices(Configuration);
+            services.AddMultiChainCoreRpcServices(rpcOptions =>
+            {
+                rpcOptions.ChainAdminAddress = "1F5WvCAwc9sX6ZMJKE8GRZA6UQQPnxgB8FbU7t";
+                rpcOptions.ChainBurnAddress = "1XXXXXXXDXXXXXXX7bXXXXXXVsXXXXXXXCT2tS";
+                rpcOptions.ChainPassword = "EmjbtUBfbg2SikAXVnAJSQKmbeZhxbpSopsVKk58zrE";
+                rpcOptions.ChainUsername = "multichainrpc";
+                // localhost or a remote node's address eg. NewChain@192.168.1.1:7764
+                rpcOptions.ChainHostname = "localhost";
+                rpcOptions.ChainName = "NewChain";
+                rpcOptions.ChainRpcPort = 7764;
+                // default is HTTP if this is left empty
+                rpcOptions.ChainSslPath = string.Empty;
+                rpcOptions.ChainUseSsl = false;
+            });
 
-            services.AddMultiChainCoreCliServices(Configuration);
+            services.AddMultiChainCoreCliServices(cliOptions =>
+            {
+                cliOptions.ChainAdminAddress = "1F5WvCAwc9sX6ZMJKE8GRZA6UQQPnxgB8FbU7t";
+                cliOptions.ChainBurnAddress = "1XXXXXXXDXXXXXXX7bXXXXXXVsXXXXXXXCT2tS";
+                cliOptions.ChainName = "NewChain";
+                // if empty default location is used
+                cliOptions.ChainDefaultColdNodeLocation = string.Empty;
+                cliOptions.ChainDefaultLocation = string.Empty;
+                cliOptions.ChainBinaryLocation = string.Empty;
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
